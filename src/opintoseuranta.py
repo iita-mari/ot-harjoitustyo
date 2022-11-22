fa2e53d57f051593fb4c1522a4dba60ebda124bb
Iita-Mari <iita-mari.ruponen@helsinki.fi>
2022-11-22 20:13:46 +0200

    Lisää viikon harjoitustyön tuotoksia

--- src/opintoseuranta.py
+++ src/opintoseuranta.py
@@ -1,17 +1,38 @@
-class Opintoseuranta:
+class Ui:
 
-    kayttajatunnus = "nakki"
-    salasana = "muusi"
+    print("~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~")
+    print("Tervetuloa opintoseuranta-appiin")
+    print("~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~")
+    print("")
 
-    minne = int (input("1. vie kirjautumaan: "))
-    if minne == 1:
-        kayttajatunnus = input("Anna käyttäjätunnus: ")
-        salasana = input("Anna salasana: ")
+    while (True):
+        print("Valitse toiminto:")
+        print("[1] Kirjaudu sisään")
+        print("[2] Luo uusi käyttäjä")
+        print("[x] Poistu")
+        print("")
 
-        if kayttajatunnus == "nakki":
-            if salasana == "muusi":
-                print("Kirjautuminen onnistui!")
-        else:
-            print("Väärin meni!")
+        user_choice = input("")
 
+        if user_choice == "1":
+            user_name = input("Anna käyttäjätunnus: ")
+            password = input("Anna salasana: ")
+            print("")
 
+        elif user_choice == "2":
+            while True:
+                new_username = input("Anna käyttäjätunnus: ")
+                new_password = input("Anna salasana: ")
+                new_password_again = input("Anna salasana uudestaan: ")
+
+                if new_password != new_password_again:
+                    print("Salasanat eivät olleet samat!")
+                    continue
+                elif new_password == new_password_again:
+                    print("Käyttäjän luonti onnistui!")
+                    print("")
+                    break
+            continue
+        elif user_choice== "x" or "X":
+            print("Kiitos ja näkemiin!")
+            break
